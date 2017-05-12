54ebca48271d1705eab9be67b62a377e18bdbd9b
zarkon4631 <zarkon4631@mail.ru>
2017-05-12 17:48:56 +0700

    Added calculation function

--- main.c
+++ main.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int CheckInput(int time, int sum)
+int CheckInput(int depDay, int sum)
 {
-	if ((time >= 0) && (time <=365) && (sum >= 10000))
+	if ((depDay >= 0) && (depDay <= 365) && (sum >= 10000))
 		return 1;
 	return 0;
 }
 
+int CalcSumDeposit(int depDay, int sum)
+{
+	float bonusPercent = 0;
+	if (sum >= 100000)
+		bonusPercent = 0.01;
+	if (depDay <= 30)
+	{
+		sum *= 0.9;
+	}
+	else if (depDay <= 120)
+	{
+		sum *= 1.0 + 0.02 + bonusPercent * 1;
+	}
+	else if (depDay <= 240)
+	{
+		sum *= 1.0 + 0.6 + bonusPercent * 2;
+	}
+	else if (depDay <= 365)
+	{
+		sum *= 1.0 + 0.12 + bonusPercent * 3;
+	}
+	return sum;
+}
+
 int main()
 {
-	int time, sum, percent;
-	printf("Введите срок влада:");
-	scanf("%d", &time);
-	printf("Введите сумму вклада:");
+	int depDay, sum;
+	printf("Введите срок влада: ");
+	scanf("%d", &depDay);
+	printf("Введите сумму вклада: ");
 	scanf("%d", &sum);
-	
-	if (CheckInput(time,sum) == 0)
+
+	if (CheckInput(depDay, sum) == 0)
 	{
 		printf("Некоректный ввод\n");
-		return 0;
+		return 1;
 	}
+
+	printf("%d", CalcSumDeposit(depDay, sum));
 
 	return 0;
 }
