0dedf93595b9e99e8ebb47747c66162b70ad3b46
zarkon4631 <zarkon4631@mail.ru>
2017-05-12 17:44:44 +0700

    Added Check Input function

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int CheckInput(int time, int sum)
+{
+	if ((time >= 0) && (time <=365) && (sum >= 10000))
+		return 1;
+	return 0;
+}
+
 int main()
 {
 	int time, sum, percent;
